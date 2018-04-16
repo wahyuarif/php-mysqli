dfd7619b6c93e45be813defc3b06de264d0c484f
Wahyu Arif Kurniawan <wahyu4rifkurniawan@gmail.com>
2018-04-16 11:36:53 +0700

    membuat form di index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<form>
+	<table>
+		<tr>
+		<td>Nama</td>
+		<td>:</td>
+		<td><input type="text" name="nama"></td>
+	</tr>
+	<tr>
+		<td>Alamat</td>
+		<td>:</td>
+		<td><input type="text" name="alamat"></td>
+	</tr>
+	<tr>
+		<td><input type="submit" name="proses" value="Proses"></td>
+	</tr>
+	</table>
+</form>
