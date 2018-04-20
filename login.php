ca1449f576b7e9375ed9670b88ef38c316d7cb4c
Wahyu Arif Kurniawan <wahyu4rifkurniawan@gmail.com>
2018-04-20 23:22:13 +0700

    add login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<form action="">
+<table>
+<tr>
+    <td>Username</td>
+    <td>:</td>
+    <td><input type="text" name="username"></td>
+</tr>
+<tr>
+    <td>Password</td>
+    <td>:</td>
+    <td><input type="password" name="password"></td>
+</tr>
+<tr>
+    <td><input type="submit" name="submit" Value="Login"></td>
+</tr>
+</table>
+</form>
